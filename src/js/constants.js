498b1c26cea0e656f4dfcb7b16a2688840ec2d2c
git <techdude1011@gmail.com>
2024-12-01 17:04:07 +0000

    made api URL configurable

--- src/js/constants.js
+++ src/js/constants.js
@@ -1,2 +1,2 @@
-export const DATASERVICE_BASE_URL = "http://192.168.1.71:8080";
+export const DATASERVICE_BASE_URL = process.env.API_URL;
 export const DATASERVICE_PATH_TODOS = "/todos/1";
--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,37 @@
-const path = require('path');
+const path = require("path");
+const webpack = require("webpack");
 
-module.exports = {
-  mode: 'development',
-  entry: './src/js/index.js',
-  devServer: {
-    static: './dist',
-  },
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(png|jpe?g|gif|svg)$/i,
-        use: [
-          {
-            loader: 'file-loader',
-          },
-        ],
-      },
-    ]
-  }
+module.exports = (env) => {
+
+  return {
+    mode: "development",
+    entry: "./src/js/index.js",
+    devServer: {
+      static: "./dist",
+    },
+    output: {
+      filename: "[name].bundle.js",
+      path: path.resolve(__dirname, "dist"),
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(png|jpe?g|gif|svg)$/i,
+          use: [
+            {
+              loader: "file-loader",
+            },
+          ],
+        },
+      ],
+    },
+    plugins: [
+      new webpack.DefinePlugin({
+        "process.env": {
+          NODE_ENV: JSON.stringify("development"),
+          API_URL: JSON.stringify(env.API_URL),
+        },
+      }),
+    ],
+  };
 };
